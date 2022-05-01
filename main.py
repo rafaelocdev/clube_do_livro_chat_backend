300d8b1e0e18d12a08b274aabc60c8e11de37aaf
Rafael Carvalho <rafaeloc.dev@gmail.com>
2022-05-01 16:55:57 -0300

    feat: created join_room and send message events

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO, join_room
 from os import getenv
 from datetime import datetime
 
@@ -7,6 +8,12 @@ app = Flask(__name__)
 
 app.debug = True
 app.config["SECRET_KEY"] = getenv("SECRET_KEY")
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("SQLALCHEMY_DATABASE_URI")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JSON_SORT_KEYS"] = False
+db = SQLAlchemy(app)
 
 socket_io = SocketIO(app, cors_allowed_origins="*")
 
@@ -21,15 +28,26 @@ def test_disconnect():
     print("Client disconnected")
 
 
-@socket_io.on("message")
-def test_send_message(message, sender, reciever):
-    print(100 * "#")
-    print(f"{message=}")
-    print(f"{sender=}")
-    print(f"{reciever=}")
-    print(f"{datetime.now()=}")
-    print(100 * "#")
+@socket_io.on("join_room")
+def handle_join_room_event(data):
+    app.logger.info(
+        "{} has joined the room {} with {}".format(
+            data["sender_id"], data["room"], data["receiver_id"]
+        )
+    )
+    join_room(data["room"])
+
+
+@socket_io.on("send_message")
+def send_message(data):
+
+    app.logger.info(
+        "{} has sent message to the room {}: {}".format(
+            data["sender_id"], data["room"], data["message"]
+        )
+    )
+    socket_io.emit("receive_message", data, room=data["room"])
 
 
 if __name__ == "__main__":
-    socket_io.run(app, host="0.0.0.0", port=5000, debug=True)
+    socket_io.run(app, host="0.0.0.0", port=5001, debug=True)
