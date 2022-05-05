d4fd1000961f6d30e71450646d1948bd95901617
Rafael Carvalho <65735479+rafaelocdev@users.noreply.github.com>
2022-05-05 00:16:05 -0300

    feat: added flask cors

--- chat.py
+++ chat.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_socketio import SocketIO, join_room
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.debug = True
 app.config["SECRET_KEY"] = getenv("SECRET_KEY")
